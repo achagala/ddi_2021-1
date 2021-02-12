c144a8c0250f8f7aa8722ed7334aeb1f11c93389
Alberto Chagala <42424250+achagala@users.noreply.github.com>
2021-02-12 00:04:47 -0800

    Práctica 1
    Se implementaron los métodos Numbers (para cada elemento encuentra cuántos números en el array son menores que dicho elemento) y PrintArray (imprime un array).

--- Práctica_1/Assets/Code/Prac_1.cs
+++ Práctica_1/Assets/Code/Prac_1.cs
@@ -7,11 +7,39 @@ public class Prac_1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Prueba Práctica 1");
+        int[] numbers = {8,1,2,2,3};
+        int[] output = Numbers(numbers);
+
+        PrintArray(output);
     }
 
-    private int[] Numbers(int[] numbers)
+    private int[] Numbers(int[] list)
     {
-        return numbers;
+        int count = 0;
+        int max = list.Length;
+        int[] output = new int[max];
+
+        for(int i = 0 ; i < max ; i++)
+        {
+            for(int j = 0 ; j < max ; j++)
+            {
+                if(list[j] < list[i])
+                {
+                    count++;
+                }
+            }
+            output[i] = count;
+            count = 0;
+            
+        }
+        return output;
+    }
+
+    private void PrintArray(int[] array)
+    {
+        foreach (var i in array)
+        {
+            Debug.Log(i);
+        }
     }
 }
