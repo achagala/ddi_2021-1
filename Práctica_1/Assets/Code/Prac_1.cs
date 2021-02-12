dbcca5b5328c305b1809f3d9ec70c8831418aaa7
Alberto Chagala <42424250+achagala@users.noreply.github.com>
2021-02-11 19:26:26 -0800

    Práctica 1
    Se creó el proyecto Práctica 1.

---
+++ Práctica_1/Assets/Code/Prac_1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prac_1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    private int[] Numbers(int[] numbers)
+    {
+
+    }
+}
