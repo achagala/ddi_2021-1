ae73272aa93549303f390f6a21258ba0c0bef5b6
Alberto Chagala <42424250+achagala@users.noreply.github.com>
2021-04-08 18:12:51 -0700

    Practica 6.2
    Practica 6.2

---
+++ Practica_1/Assets/VirtualButtonScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject pikachuButton;
+    public Animator pikachuAnimation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pikachuButton = GameObject.Find("PikachuButton");
+        pikachuButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        pikachuAnimation.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        pikachuAnimation.Play("PikachuAnimation");
+        Debug.Log("Btn pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        pikachuAnimation.Play("None");
+        Debug.Log("Btn released");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
